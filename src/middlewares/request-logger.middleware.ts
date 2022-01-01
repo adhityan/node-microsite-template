398d8b967617702624591f9dc6b816e578196001
Adhityan KV <adhityan@hotmail.com>
2022-01-01 13:21:13 +0400

    chore: support for new typedi version

--- src/middlewares/request-logger.middleware.ts
+++ src/middlewares/request-logger.middleware.ts
@@ -1,6 +1,9 @@
+import { Service } from 'typedi';
 import { Middleware, ExpressMiddlewareInterface } from 'routing-controllers';
+
 import { Logger } from '@adhityan/gc-logger';
 
+@Service()
 @Middleware({ type: 'before' })
 export class RequestLoggerMiddleWare implements ExpressMiddlewareInterface {
     // eslint-disable-next-line class-methods-use-this
